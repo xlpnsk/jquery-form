81bb9556dba036c8baad60a0f880b555c86902d4
alipinski <adam.lipinski@dogtronic.io>
2022-08-16 15:25:20 +0200

    Init project

---
+++ js/index.js
@@ -0,0 +1,89 @@
+const availableTags = [
+  'New',
+  'Hit of the day',
+  'Special offer',
+  'Last items in stock',
+  'Damaged',
+]
+
+function addProduct() {
+  var form = $('#form')
+  var actionUrl = form.attr('action')
+  $.ajax({
+    type: 'POST',
+    url: actionUrl,
+    dataType: 'json',
+    data: form.serialize(),
+    success: function (data) {
+      console.log(data)
+    },
+    error: function (xhr, resp, text) {
+      console.log(xhr, resp, text)
+      form.append(
+        `<p class="form-error">An error occurred while adding a product. Please try again later.</p>`
+      )
+    },
+    complete: function(xhr, status){
+      console.log(xhr);
+    }
+  })
+}
+
+$(function () {
+  dialog = $('#add-modal').dialog({
+    autoOpen: false,
+    height: 700,
+    width: 800,
+    modal: true,
+    buttons: {
+      'Add a new product': addProduct,
+      Cancel: function () {
+        dialog.dialog('close')
+      },
+    },
+    close: function () {
+      form[0].reset()
+    },
+  })
+
+  form = dialog.find('form').on('submit', function (event) {
+    event.preventDefault()
+    addUser()
+  })
+
+  $('#modal-open')
+    .button()
+    .on('click', function () {
+      dialog.dialog('open')
+    })
+
+  function split(val) {
+    return val.split(/,\s*/)
+  }
+  function extractLast(term) {
+    return split(term).pop()
+  }
+
+  $('#tags').autocomplete({
+    minLength: 0,
+    source: function (request, response) {
+      response(
+        $.ui.autocomplete.filter(availableTags, extractLast(request.term))
+      )
+    },
+    focus: function () {
+      return false
+    },
+    select: function (event, ui) {
+      var terms = split(this.value)
+      // remove the current input
+      terms.pop()
+      // add the selected item
+      terms.push(ui.item.value)
+      // add placeholder to get the comma-and-space at the end
+      terms.push('')
+      this.value = terms.join(', ')
+      return false
+    },
+  })
+})
